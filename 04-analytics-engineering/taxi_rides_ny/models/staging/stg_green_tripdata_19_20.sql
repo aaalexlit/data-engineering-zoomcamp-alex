69c6942bdec1d57a2cd53e85301735e66f1e8f43
aaalex.lit@gmail.com <aaalex.lit@gmail.com>
2024-02-23 14:29:14 +0000

    correct columns in the green staging table

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata_19_20.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata_19_20.sql
@@ -6,33 +6,34 @@ with
         where vendorid is not null
     )
 select
-    {{ dbt_utils.generate_surrogate_key(["vendorid", "lpep_pickup_datetime"]) }}
-    as tripid,
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
     {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }} as vendorid,
+    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }} as ratecodeid,
+    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+    -- timestamps
     cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
     cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
     store_and_fwd_flag,
-    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }} as ratecodeid,
     {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
+    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
+
+    -- payment info
     cast(fare_amount as numeric) as fare_amount,
     cast(extra as numeric) as extra,
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
     cast(ehail_fee as numeric) as ehail_fee,
-    cast(airport_fee as numeric) as airport_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
-    {{ get_payment_type_description(payment_type) }} as payment_type_description,
-    distance_between_service,
-    time_between_service,
-    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
-    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
-    data_file_year,
-    data_file_month
+    {{ get_payment_type_description("payment_type") }} as payment_type_description
 from tripdata
 where rn = 1
 
